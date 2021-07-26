b93a1afc0327daf1a6064b160daa4b5a33055d8e
matttMP <66797927+matttMP@users.noreply.github.com>
2021-07-25 20:46:25 -0500

    Create Source.bat

---
+++ Source.bat
@@ -0,0 +1,7 @@
+@ECHO OFF
+ECHO Patching Cursor... Please Wait...
+copy %LocalAppData%\Roblox\Versions\version-LATESTROBLOXVERSION\content\textures\ArrowCursor.png %LocalAppData%\Roblox\Versions\version-f98ef77f473148f6\content\textures\Cursors\KeyboardMouse\ArrowCursor.png
+copy %LocalAppData%\Roblox\Versions\version-LATESTROBLOXVERSION\content\textures\ArrowFarCursor.png %LocalAppData%\Roblox\Versions\version-f98ef77f473148f6\content\textures\Cursors\KeyboardMouse\ArrowFarCursor.png
+copy %LocalAppData%\Roblox\Versions\version-LATESTROBLOXVERSION\content\textures\ArrowCursorDecalDrag.png %LocalAppData%\Roblox\Versions\version-f98ef77f473148f6\content\textures\Cursors\KeyboardMouse\IBeamCursor.png
+ECHO Finished Patching Cursor! Enjoy playing Roblox!
+PAUSE
